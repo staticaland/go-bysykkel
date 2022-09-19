047bab95422e7b874c971d36de7f4275af8e5f82
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-19 14:20:28 +0200

    chore: Couple of tests and a small refactoring

---
+++ cmd/bysykkel-server/handlers_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/rs/zerolog"
+
+	"github.com/staticaland/go-bysykkel/internal/assert"
+	"github.com/staticaland/go-bysykkel/internal/client"
+	"github.com/staticaland/go-bysykkel/internal/models"
+)
+
+func TestHealthCheck(t *testing.T) {
+
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app := application{}
+
+	app.healthCheckHandler(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+}
+
+func TestShowStations(t *testing.T) {
+
+	logger := zerolog.New(io.Discard)
+
+	c := client.CreateClient()
+
+	app := &application{
+		logger:   logger,
+		stations: &models.StationModel{Client: c},
+	}
+
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/vqwewqe", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app.healthCheckHandler(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/staticaland/go-bysykkel
 go 1.18
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/rs/zerolog v1.28.0
+)
+
+require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/rs/zerolog v1.28.0 // indirect
 	golang.org/x/sys v0.0.0-20220915200043-7b5979e65e41 // indirect
 )
